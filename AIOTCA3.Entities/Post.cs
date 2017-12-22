9c9d418182b823f0f751b0457f77acc891041943
Julian Alwandy (IT Alliance Ltd) <v-jualwa@microsoft.com>
2017-12-22 20:05:34 +0100

    Updated dashboard

--- AIOTCA3.Entities/Post.cs
+++ AIOTCA3.Entities/Post.cs
@@ -16,9 +16,8 @@ namespace AIOTCA3.Entities
         [DataType(DataType.MultilineText)]
         [MinLength(500)]
         public string Body { get; set; }
-
         [Required]
-        public string Author { get; set; }
+        public string Author { get; set; } = HttpContext.Current.User.Identity.Name;
 
         public DateTime Date { get; set; }
     }
